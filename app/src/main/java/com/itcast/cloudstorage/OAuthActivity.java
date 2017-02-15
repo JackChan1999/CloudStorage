9c27c165285839b2737a40ead818615ca945aca9
JackChen1999 <jackychan2040@gmail.com>
2017-02-15 14:40:43 +0800

    修改点9图

--- app/src/main/java/com/itcast/cloudstorage/OAuthActivity.java
+++ app/src/main/java/com/itcast/cloudstorage/OAuthActivity.java
@@ -18,7 +18,26 @@ import com.vdisk.net.exception.VDiskException;
 import com.vdisk.net.session.AccessToken;
 import com.vdisk.net.session.AppKeyPair;
 import com.vdisk.net.session.Session.AccessType;
-
+/**
+ * ============================================================
+ * Copyright：${TODO}有限公司版权所有 (c) 2017
+ * Author：   陈冠杰
+ * Email：    815712739@qq.com
+ * GitHub：   https://github.com/JackChen1999
+ * 博客：     http://blog.csdn.net/axi295309066
+ * 微博：     AndroidDeveloper
+ * <p>
+ * Project_Name：CloudStorage
+ * Package_Name：com.itcast.cloudstorage
+ * Version：1.0
+ * time：2017/2/15 14:33
+ * des ：${TODO}
+ * gitVersion：$Rev$
+ * updateAuthor：$Author$
+ * updateDate：$Date$
+ * updateDes：${TODO}
+ * ============================================================
+ **/
 public class OAuthActivity extends SherlockActivity implements
 		VDiskDialogListener {
 	Button btn_oauth;
